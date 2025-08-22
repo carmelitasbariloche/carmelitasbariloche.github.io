f2a35ca319146f36556bc24a3fb8bc2e1237fd5c
aragonl <aragon_l@hotmail.com>
2025-08-22 15:12:41 -0300

    fixed timezone

--- src/render.csv.py
+++
@@ -1,8 +0,0 @@
-from jinja2 import Template, Environment,FileSystemLoader
-from pathlib import Path
-
-fname='../data/wc-product-export-24-2-2021.csv'
-from myfuncs import ReadCSV
-catalog=ReadCSV(fname)
-
-catalog.sort(key=lambda k: k['categorias'])
